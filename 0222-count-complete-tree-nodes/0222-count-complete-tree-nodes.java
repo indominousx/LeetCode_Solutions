74e3d07a429405b3655262a313e30612a1afe25d
Krishna Gandhi <149898173+indominousx@users.noreply.github.com>
2026-07-02 12:55:50 +0530

    Time: 0 ms (100%), Space: 49.5 MB (47.9%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -15,15 +15,37 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        int cnt=0;
-        List<Integer> arr = new ArrayList<>();
-        findNodes(root,cnt,arr);
-        return arr.size();
+        // int cnt=0;
+        // List<Integer> arr = new ArrayList<>();
+        // findNodes(root,cnt,arr);
+        // return arr.size();
+        if(root==null) return 0;
+        int l= getlh(root);
+        int r = getrh(root);
+        if(l==r) return ((1<<l)-1);
+        else return countNodes(root.left) + countNodes(root.right)+1;
     }
-       public void findNodes(TreeNode root , int cnt , List<Integer> arr){
-         if(root==null) return;
-         arr.add(root.val);
-         findNodes(root.left,cnt,arr);
-         findNodes(root.right,cnt,arr);
+    public int getlh(TreeNode root){
+        int cnt=0;
+        while(root!=null){
+            cnt+=1;
+            root=root.left;
+        }
+        return cnt;
+    }
+    public int getrh(TreeNode root){
+       int cnt=0;
+       while(root!=null){
+        cnt+=1;
+        root=root.right;
        }
+       return cnt;
+    } 
+    //    public void findNodes(TreeNode root , int cnt , List<Integer> arr){
+    //      if(root==null) return;
+    //      arr.add(root.val);
+    //      findNodes(root.left,cnt,arr);
+    //      findNodes(root.right,cnt,arr);
+    //    }
+    
 }
