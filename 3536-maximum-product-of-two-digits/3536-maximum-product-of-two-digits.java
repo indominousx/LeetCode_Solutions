53c283d053e93464a8e4b58391718587f2fecb7f
Krishna Gandhi <149898173+indominousx@users.noreply.github.com>
2026-07-25 11:03:00 +0530

    Time: 2 ms (29.84%), Space: 42.9 MB (23.31%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -3,7 +3,20 @@ class Solution {
         String s = n + "";
         if(s.length()==2) return (s.charAt(0)-'0')*(s.charAt(1)-'0');
         char[] arr = s.toCharArray();
-        Arrays.sort(arr);
-        return (arr[arr.length-1]-'0') * (arr[arr.length-2]-'0') ;
+        // Arrays.sort(arr);
+        // return (arr[arr.length-1]-'0') * (arr[arr.length-2]-'0') ;
+        int max1=0;
+        int max2=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)-'0' > max1){
+                max2=max1;
+                max1=s.charAt(i)-'0';
+                
+            }
+            else if(s.charAt(i)-'0' > max2){
+                max2=s.charAt(i)-'0';
+            }
+        }
+        return max1*max2;
     }
 }
