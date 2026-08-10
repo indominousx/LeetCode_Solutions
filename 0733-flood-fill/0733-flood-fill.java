cb9200ad0fec653169444db606e534e41cbf92df
Krishna Gandhi <149898173+indominousx@users.noreply.github.com>
2026-08-10 18:23:09 +0530

    Time: 1 ms (38.43%), Space: 46.6 MB (79.96%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,22 +1,40 @@
 class Solution {
-    public void dfs(int[][] i, int[][] ans , int r , int c , int color , int[] delrow , int[] delcol , int inicol){
-        ans[r][c]=color;
-        int m=i.length;
-        int n=i[0].length;
-        for(int j=0;j<4;j++){
-            int nr=r+delrow[j];
-            int nc=c+delcol[j];
-            if(nr>=0 && nr<m && nc>=0 && nc<n && i[nr][nc]==inicol && ans[nr][nc]!=color){
-                dfs(i,ans,nr,nc,color,delrow,delcol,inicol);
-            }
-        }
-    }
+    // public void dfs(int[][] i, int[][] ans , int r , int c , int color , int[] delrow , int[] delcol , int inicol){
+    //     ans[r][c]=color;
+    //     int m=i.length;
+    //     int n=i[0].length;
+    //     for(int j=0;j<4;j++){
+    //         int nr=r+delrow[j];
+    //         int nc=c+delcol[j];
+    //         if(nr>=0 && nr<m && nc>=0 && nc<n && i[nr][nc]==inicol && ans[nr][nc]!=color){
+    //             dfs(i,ans,nr,nc,color,delrow,delcol,inicol);
+    //         }
+    //     }
+    // }
     public int[][] floodFill(int[][] i, int sr, int sc, int color) {
         int inicol=i[sr][sc];
         int[][] ans = i;
         int[] delrow={-1,0,1,0};
         int[] delcol={0,1,0,-1};
-        dfs(i,ans,sr,sc,color,delrow,delcol,inicol);
+        int m=i.length;
+        int n=i[0].length;
+        // dfs(i,ans,sr,sc,color,delrow,delcol,inicol);
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{sr,sc});
+        ans[sr][sc]=color;
+        while(!q.isEmpty()){
+            int[] curr=q.poll();
+            int x=curr[0];
+            int y=curr[1];
+            for(int j=0;j<4;j++){
+                int nr=x+delrow[j];
+                int nc=y+delcol[j];
+                 if(nr>=0 && nr<m && nc>=0 && nc<n && i[nr][nc]==inicol && ans[nr][nc]!=color){
+                  ans[nr][nc]=color;
+                  q.add(new int[]{nr,nc});
+                 }
+            }
+        }
         return ans;
     }
 }
