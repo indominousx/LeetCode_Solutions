21ae91634a4fb569c79973fa911768f516033c58
Krishna Gandhi <149898173+indominousx@users.noreply.github.com>
2026-08-13 11:46:03 +0530

    Time: 14 ms (69.92%), Space: 49.5 MB (21.42%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,48 @@
+class Node{
+    int xcor;
+    int ycor;
+    int dist;
+    Node(int xcor , int ycor , int dist){
+        this.xcor=xcor;
+        this.ycor=ycor;
+        this.dist=dist;
+    }
+}
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+     int m=mat.length;
+     int n=mat[0].length;
+     int[][] vis = new int[m][n];
+     int[][] distance = new int[m][n];
+     Queue<Node> q = new LinkedList<>();
+     for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(mat[i][j]==0){
+                q.add(new Node(i,j,0));
+                vis[i][j]=1;
+            }
+            else{
+                vis[i][j]=0;
+            }
+        }
+     }  
+     int[] drow={-1,0,1,0};
+     int[] dcol={0,1,0,-1};
+     while(!q.isEmpty()){
+        Node curr = q.poll();
+        int x=curr.xcor;
+        int y=curr.ycor;
+        int d=curr.dist;
+        distance[x][y]=d;
+        for(int i=0;i<4;i++){
+            int nrow=x+drow[i];
+            int ncol=y+dcol[i];
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0){
+                vis[nrow][ncol]=1;
+                q.add(new Node(nrow,ncol,d+1));
+            }
+        }
+     } 
+     return distance;
+    }
+}
