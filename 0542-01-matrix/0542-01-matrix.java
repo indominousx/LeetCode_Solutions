7dc1bf71da0e75ce56282973b424bd23d9cbb579
Krishna Gandhi <149898173+indominousx@users.noreply.github.com>
2026-08-29 12:19:43 +0530

    Time: 14 ms (71.42%), Space: 49.9 MB (8.3%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,48 +1,45 @@
 class Node{
-    int xcor;
-    int ycor;
-    int dist;
-    Node(int xcor , int ycor , int dist){
-        this.xcor=xcor;
-        this.ycor=ycor;
-        this.dist=dist;
+    int x;
+    int y;
+    int dis;
+    Node(int x , int y , int dis){
+        this.x=x;
+        this.y=y;
+        this.dis=dis;
     }
 }
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-     int m=mat.length;
-     int n=mat[0].length;
-     int[][] vis = new int[m][n];
-     int[][] distance = new int[m][n];
-     Queue<Node> q = new LinkedList<>();
-     for(int i=0;i<m;i++){
+      int m=mat.length;
+      int n=mat[0].length;
+      Queue<Node> q = new LinkedList<>();
+      int[][] dist= new int[m][n];
+      int[][] vis= new int[m][n];
+      for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             if(mat[i][j]==0){
                 q.add(new Node(i,j,0));
                 vis[i][j]=1;
             }
-            else{
-                vis[i][j]=0;
-            }
         }
-     }  
-     int[] drow={-1,0,1,0};
-     int[] dcol={0,1,0,-1};
-     while(!q.isEmpty()){
+      }
+      int[] xcor = new int[]{-1,0,1,0};
+      int[] ycor = new int[]{0,1,0,-1};
+      while(!q.isEmpty()){
         Node curr = q.poll();
-        int x=curr.xcor;
-        int y=curr.ycor;
-        int d=curr.dist;
-        distance[x][y]=d;
+        int x = curr.x;
+        int y =curr.y;
+        int dis=curr.dis;
+        dist[x][y]=dis;
         for(int i=0;i<4;i++){
-            int nrow=x+drow[i];
-            int ncol=y+dcol[i];
-            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0){
-                vis[nrow][ncol]=1;
-                q.add(new Node(nrow,ncol,d+1));
+            int nx  = x+xcor[i];
+            int ny= y+ycor[i];
+            if(nx>=0 && nx<m && ny>=0 && ny<n && vis[nx][ny]==0 && mat[nx][ny]==1){
+                q.add(new Node(nx,ny,dis+1));
+                vis[nx][ny]=1;
             }
         }
-     } 
-     return distance;
+      } 
+      return dist;
     }
 }
